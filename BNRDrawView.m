a7c671eed2bad0c82e687bb77d5f30197280c771
alex <abastide@gmail.com>
2014-09-15 14:40:00 +0800

    - implementation of chapter 13

--- BNRDrawView.m
+++ BNRDrawView.m
@@ -9,10 +9,13 @@
 #import "BNRDrawView.h"
 #import "BNRLine.h"
 
-@interface BNRDrawView ()
+@interface BNRDrawView () <UIGestureRecognizerDelegate>
 
+@property (nonatomic, strong) UIPanGestureRecognizer *moveRecognizer;
 @property (nonatomic, strong) NSMutableDictionary *linesInProgess;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@property (nonatomic, weak) BNRLine *selectedLine;
 
 @end
 
@@ -30,8 +33,31 @@
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
+        
+        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+        tapRecognizer.delaysTouchesBegan = YES;
+        [tapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+        [self addGestureRecognizer:tapRecognizer];
+        
+        UILongPressGestureRecognizer *longPressRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPress:)];
+        [self addGestureRecognizer:longPressRecognizer];
+        
+        self.moveRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveLine:)];
+        self.moveRecognizer.delegate = self;
+        self.moveRecognizer.cancelsTouchesInView = NO;
+        [self addGestureRecognizer:self.moveRecognizer];
     }
     return self;
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
 }
 
 #pragma mark - drawing management
@@ -60,9 +86,129 @@
      // If there is a line currently being drawn, draw it in red
         [self strokeLine:self.linesInProgess[key]];
     }
+    if (self.selectedLine)
+    {
+        [[UIColor greenColor] set];
+        [self strokeLine:self.selectedLine];
+    }
+}
+
+- (BNRLine *)lineAtPoint:(CGPoint)p
+{
+    // Find a line close to p
+    for (BNRLine *l in self.finishedLines)
+    {
+        CGPoint start = l.begin;
+        CGPoint end = l.end;
+        // Check a few points on the line
+        for (float t = 0.0; t <= 1.0; t += 0.05)
+        {
+            float x = start.x + t * (end.x - start.x);
+            float y = start.y + t * (end.y - start.y);
+            // If the tapped point is within 20 points, let's return this line
+            if (hypot(x - p.x, y - p.y) < 20.0)
+            {
+                return l;
+            }
+        }
+    }
+    // If nothing is close enough to the tapped point, then we did not select the point;
+    return nil;
+}
+
+- (void)moveLine:(UIPanGestureRecognizer *)gr
+{
+    if (!self.selectedLine)
+    {
+        return;
+    }
+    
+    if (gr.state == UIGestureRecognizerStateChanged)
+    {
+        CGPoint translation = [gr translationInView:self];
+        CGPoint begin = self.selectedLine.begin;
+        CGPoint end = self.selectedLine.end;
+        begin.x += translation.x;
+        begin.y += translation.y;
+        end.x += translation.x;
+        end.y += translation.y;
+        
+        self.selectedLine.begin = begin;
+        self.selectedLine.end = end;
+        
+        [self setNeedsDisplay];
+        [gr setTranslation:CGPointZero inView:self];
+    }
 }
 
 #pragma mark - touch event management
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+{
+    if (gestureRecognizer == self.moveRecognizer)
+    {
+        return YES;
+    }
+    return NO;
+}
+
+- (void)longPress:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized tap");
+
+    if (gr.state == UIGestureRecognizerStateBegan)
+    {
+        CGPoint point = [gr locationInView:self];
+        self.selectedLine = [self lineAtPoint:point];
+        if (self.selectedLine)
+        {
+            [self.linesInProgess removeAllObjects];
+        }
+    }
+    else if (gr.state == UIGestureRecognizerStateEnded)
+    {
+        self.selectedLine = nil;
+    }
+    [self setNeedsDisplay];
+}
+
+- (void)tap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized tap");
+    
+    CGPoint point = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+    if (self.selectedLine)
+    {
+        // Make ourselves the target of menu item action messages
+        [self becomeFirstResponder];
+        
+        // Grab the menu controller
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        
+        // Create a new "Delete" UIMenuItem
+        UIMenuItem *deleteItem = [[UIMenuItem alloc] initWithTitle:@"Delete" action:@selector(deleteLine:)];
+        menu.menuItems = @[deleteItem];
+        
+        // Tell the mnu where it should come from and show it
+        [menu setTargetRect:CGRectMake(point.x, point.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    }
+    else
+    {
+        // Hide the menu if no line is selected
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+    }
+    [self setNeedsDisplay];
+}
+
+- (void)doubleTap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Double Tap");
+    [self.linesInProgess removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+}
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
@@ -121,6 +267,15 @@
         NSValue *key = [NSValue valueWithNonretainedObject:t];
         [self.linesInProgess removeObjectForKey:key];
     }
+    [self setNeedsDisplay];
+}
+
+#pragma mark - Data management
+
+- (void)deleteLine:(id)sender
+{
+    // Remove the selected line from the list of _finishedLines
+    [self.finishedLines removeObject:self.selectedLine];
     [self setNeedsDisplay];
 }
 
